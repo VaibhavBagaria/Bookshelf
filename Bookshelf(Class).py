8a9e9e3fb3b7531666eeb07e1212bb8c9be37c4f
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2022-03-02 21:31:48 -0300

    Add files via upload

--- Bookshelf(Class).py
+++ Bookshelf(Class).py
@@ -14,13 +14,13 @@ class Bookshelf:
     
     def year_since_published(self):
         years_ago_publish=2022-self.citizin_publishing_year
-        print("The book was published "+years_ago_publish+" ago.")
+        print("The book was published "+str(years_ago_publish)+" years ago.")
         
-book1=Bookshelf("Harry Potter and the Philospher's stone","J.K. Rowling","R$ 57.13","1997")
+book1=Bookshelf("Harry Potter and the Philospher's stone","J.K. Rowling","R$ 57.13",1997)
 book1.addbook()
 book1.year_since_published()
 
-book2=Bookshelf("Harry Potter and the Order of Pheonix","J.K. Rowling","R$ 295.51","2007")
+book2=Bookshelf("Harry Potter and the Order of Pheonix","J.K. Rowling","R$ 295.51",2007)
         
 book2.addbook()
 book2.year_since_published()
